f842bd6bcf68a19a954d40f4a40400435e47fe05
ritnetsys1979 <rit.netsys1979@gmail.com>
2022-11-04 13:01:35 +0530

    v6

--- test.php
+++ test.php
@@ -1,3 +1,16 @@
 <?php
 echo "Welcome To My Site"
+   
+	$hostname=appmysql.mysql.database.azure.com
+	$username=aemadmin
+	$password="Rituparna!@#2022"
+	$ssl-mode=require
+	$dbdb = "books";
+    
+$conn = new mysqli($hostname, $username, $password, $dbdb);
+   if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+	else
+	echo "connected";
 ?>
