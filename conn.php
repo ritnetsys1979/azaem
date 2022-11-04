60006acc8b4c4e8aa7a8205965a57806d8b5bdb7
ritnetsys1979 <rit.netsys1979@gmail.com>
2022-11-04 12:53:19 +0530

    v3

---
+++ conn.php
@@ -0,0 +1,11 @@
+<?php
+    $user = "aemadmin";
+    $pass = "Rituparna!@#2022";
+    $host = "appmysql.mysql.database.azure.com";
+    $dbdb = "books";
+    
+$conn = new mysqli($host, $user, $pass, $dbdb);
+   if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+?>
